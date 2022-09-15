f1b713adf9a44d86edfb64de52d9f4fd35730fca
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-09-15 14:28:47 +0100

    Add Apollo

--- W2-github-oath/web/src/pages/_app.tsx
+++ W2-github-oath/web/src/pages/_app.tsx
@@ -1,10 +1,16 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import "tailwindcss/tailwind.css";
+import 'tailwindcss/tailwind.css';
+import { client } from '../lib/apollo';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
