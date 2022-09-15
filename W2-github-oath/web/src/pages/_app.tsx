058e10babb462d26651ec4b4b5acda3a09d56f6e
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-09-15 14:00:30 +0100

    Init web

---
+++ W2-github-oath/web/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import "tailwindcss/tailwind.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
