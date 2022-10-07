87f701231b3a722573078dfd92f0705fa78a64d2
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-10-07 14:53:06 +0100

    init web

---
+++ W5-stripe-int2/web/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => (
+  <div className='flex items-center justify-center bg-blue-500'>Hiya</div>
+);
+
+export default Home;
---
+++ W5-stripe-int2/web/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        purple1: "#181621",
+        purple2: "#7c3fc7",
+        purple3: "#9e6add",
+        purple4: "#2b273b",
+      },
+      backgroundColor: (theme) => theme("colors"),
+      padding: {
+        "n-1": "-0.25rem",
+        "n-4": "-1rem",
+        "n-8": "-2rem",
+      },
+    },
+  },
+  plugins: [],
+};
